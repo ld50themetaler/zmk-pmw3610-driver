cf00b11de2f8e1f74ff4456f464d0111a3f975d2
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2025-03-28 23:29:09 +0900

    Disable layer dependent functions for peripheral

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -547,7 +547,7 @@ static void pmw3610_async_init(struct k_work *work) {
 }
 
 #define AUTOMOUSE_LAYER (DT_PROP(DT_DRV_INST(0), automouse_layer))
-#if AUTOMOUSE_LAYER > 0
+#if AUTOMOUSE_LAYER > 0 && (!defined(CONFIG_ZMK_SPLIT) || defined(CONFIG_ZMK_SPLIT_ROLE_CENTRAL))
 struct k_timer automouse_layer_timer;
 static bool automouse_triggered = false;
 
@@ -566,6 +566,7 @@ K_TIMER_DEFINE(automouse_layer_timer, deactivate_automouse_layer, NULL);
 #endif
 
 static enum pixart_input_mode get_input_mode_for_current_layer(const struct device *dev) {
+# if !defined(CONFIG_ZMK_SPLIT) || defined(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     const struct pixart_config *config = dev->config;
     uint8_t curr_layer = zmk_keymap_highest_layer_active();
     for (size_t i = 0; i < config->scroll_layers_len; i++) {
@@ -578,6 +579,8 @@ static enum pixart_input_mode get_input_mode_for_current_layer(const struct devi
             return SNIPE;
         }
     }
+#endif
+
     return MOVE;
 }
 
@@ -616,7 +619,7 @@ static int pmw3610_report_data(const struct device *dev) {
 
     data->curr_mode = input_mode;
 
-#if AUTOMOUSE_LAYER > 0
+#if AUTOMOUSE_LAYER > 0 && (!defined(CONFIG_ZMK_SPLIT) || defined(CONFIG_ZMK_SPLIT_ROLE_CENTRAL))
     if (input_mode == MOVE &&
             (automouse_triggered || zmk_keymap_highest_layer_active() != AUTOMOUSE_LAYER)
     ) {
